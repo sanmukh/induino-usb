ddc71ffdf841de52591742d2173e3701783176d4
Sanmukh_lab <kuppanna@usc.edu>
2014-03-19 16:28:31 -0700

    removed the warnings which arose due to compiling with gcc 4.8. It seems that usb driver functions now use int instead of size_t. Also cleaned up the code to conform with the linux coding standards. Checked using the script checkpatch.pl and succeeded with 0 errors and warnings.

--- induino_usb.c
+++ induino_usb.c
@@ -14,19 +14,17 @@
 #define INDUINO_MINOR_BASE 96
 
 /*table of devices that work with this driver.*/
-struct usb_device_id id_table[]={
-	{USB_DEVICE(VENDOR_ID,PRODUCT_ID)},
+struct usb_device_id id_table[] = {
+	{USB_DEVICE(VENDOR_ID, PRODUCT_ID)},
 	{},
 };
 
-
 static struct usb_driver induino_driver;
 
-MODULE_DEVICE_TABLE(usb,id_table);
+MODULE_DEVICE_TABLE(usb, id_table);
 
-
-struct induino_usb{
-	struct usb_device* udev;
+struct induino_usb {
+	struct usb_device *udev;
 	struct usb_endpoint_descriptor *bulk_in_endpoint;
 	struct usb_endpoint_descriptor *bulk_out_endpoint;
 	int minor;
@@ -34,62 +32,65 @@ struct induino_usb{
 	int bulk_out_size;
 };
 
-static void induino_bulk_complete(struct urb* urb){
-
-	
-	if(urb->status == 0){
-		printk("Data successfully transferred\n");
-		printk("Data transferred is\n %s length: %d",(char*)urb->transfer_buffer,urb->actual_length);
-	}
-	else{
-		printk("Error in transmitting data. Error code: %d",urb->status);
+static void induino_bulk_complete(struct urb *urb)
+{
+	if (urb->status == 0) {
+		pr_debug("Data successfully transferred\n");
+		pr_debug("Data transferred is\n %s length: %d",
+			(char *)urb->transfer_buffer, urb->actual_length);
+	} else {
+		pr_debug("Error in transmitting data. Error code: %d",
+			urb->status);
 	}
 	usb_free_urb(urb);
 }
 
-
-
-static ssize_t induino_read(struct file* file,char __user *user_buf, size_t count, loff_t* ppos){
+static ssize_t induino_read(struct file *file, char __user *user_buf,
+			size_t count, loff_t *ppos)
+{
 
 	ssize_t retval = -1;
 	int subminor;
-	struct usb_interface* interface;
-	struct induino_usb* dev;
-	char* buf;
+	struct usb_interface *interface;
+	struct induino_usb *dev;
+	char *buf;
 
-	if(!user_buf){
-		printk("User buffer not allocated\n");
+	if (!user_buf) {
+		pr_debug("User buffer not allocated\n");
 		retval = -1;
 		goto error;
 	}
-	
+
 	subminor = MINOR(file->f_dentry->d_inode->i_rdev);
 
-	interface = usb_find_interface(&induino_driver,subminor);
+	interface = usb_find_interface(&induino_driver, subminor);
 
-	if(!interface){
-		printk("Could not find the device interface \n");
+	if (!interface) {
+		pr_debug("Could not find the device interface\n");
 		retval = -1;
 		goto error;
 	}
 
 	dev = usb_get_intfdata(interface);
 
-	buf = kmalloc(dev->bulk_out_size,GFP_KERNEL);
-	memset(buf,0,dev->bulk_out_size);
+	buf = kmalloc(dev->bulk_out_size, GFP_KERNEL);
+	memset(buf, 0, dev->bulk_out_size);
 
-	if(!buf){
+	if (!buf) {
 		retval = -ENOMEM;
 		goto error;
 	}
 
-	retval = (ssize_t)usb_bulk_msg(dev->udev,usb_rcvbulkpipe(dev->udev,dev->bulk_in_endpoint->bEndpointAddress),buf,min(dev->bulk_in_size,count),&count,HZ*10);
+	retval = (ssize_t)usb_bulk_msg(dev->udev,
+			usb_rcvbulkpipe(dev->udev,
+			dev->bulk_in_endpoint->bEndpointAddress),
+			buf, (int)min(dev->bulk_in_size, (int)count),
+			(int *)&count, HZ*10);
 
-	if(!retval){
-		printk("Data is: %s, data length is: %d\n",buf,count);
-		if(copy_to_user(user_buf,buf+2,count)){
+	if (!retval) {
+		pr_debug("Data is: %s, data length is: %zu\n", buf, count);
+		if (copy_to_user(user_buf, buf + 2, count))
 			retval = -EFAULT;
-		}
 		else
 			retval = count;
 	}
@@ -97,87 +98,95 @@ static ssize_t induino_read(struct file* file,char __user *user_buf, size_t coun
 error:
 	return retval;
 }
-//The write function
-static ssize_t induino_write(struct file* file,const char __user *user_buf, size_t count, loff_t* ppos){
-	ssize_t retval=-1;
+
+/*The write function*/
+static ssize_t induino_write(struct file *file,
+				const char __user *user_buf,
+				size_t count, loff_t *ppos)
+{
+	ssize_t retval = -1;
 	int submitret = -1;
 	int subminor;
-	struct usb_interface* interface;
-	struct induino_usb* dev;	
-	struct urb* out_urb;
-	char* buf;
+	struct usb_interface *interface;
+	struct induino_usb *dev;
+	struct urb *out_urb;
+	char *buf;
 
 	subminor = MINOR(file->f_dentry->d_inode->i_rdev);
 
-	interface = usb_find_interface(&induino_driver,subminor);
+	interface = usb_find_interface(&induino_driver, subminor);
 
-	if(!interface){
-		printk("Could not find the interface\n");
+	if (!interface) {
+		pr_debug("Could not find the interface\n");
 		return -1;
-	}	
+	}
 
 	dev = usb_get_intfdata(interface);
-	if(count > dev->bulk_out_size){
-		printk("Too much input data\n");
+	if (count > dev->bulk_out_size) {
+		pr_debug("Too much input data\n");
 		return retval;
-	}	
+	}
 
-	if(user_buf){
-		out_urb = usb_alloc_urb(0,GFP_KERNEL);
-		if(!out_urb){
+	if (user_buf) {
+		out_urb = usb_alloc_urb(0, GFP_KERNEL);
+		if (!out_urb) {
 			retval = -ENOMEM;
 			goto error;
 		}
-		
-		buf = kmalloc(count,GFP_KERNEL);
-		if(!buf){
+
+		buf = kmalloc(count, GFP_KERNEL);
+		if (!buf) {
 			retval = -ENOMEM;
 			goto error;
 		}
-		if(copy_from_user(buf,user_buf,count)){
+
+		if (copy_from_user(buf, user_buf, count)) {
 			retval = -EFAULT;
 			goto error;
 		}
 
-		usb_fill_bulk_urb(out_urb,dev->udev,usb_sndbulkpipe(dev->udev,dev->bulk_out_endpoint->bEndpointAddress),buf,count,induino_bulk_complete,dev);
-		submitret = usb_submit_urb(out_urb,GFP_KERNEL);
-		if(submitret){
-			printk("Error in submitting the urb: %d\n",submitret);
+		usb_fill_bulk_urb(out_urb, dev->udev,
+				usb_sndbulkpipe(dev->udev,
+				dev->bulk_out_endpoint->bEndpointAddress),
+				buf, count, induino_bulk_complete, dev);
+		submitret = usb_submit_urb(out_urb, GFP_KERNEL);
+		if (submitret) {
+			pr_debug("Error in submitting the urb: %d\n",
+				submitret);
 			retval = -1;
 		}
 
-		retval = count;	
+		retval = count;
+	} else {
+		pr_debug("User buffer is empty\n");
 	}
-	else{
-		printk("User buffer is empty\n");
-		
-	}
-	
+
 error:
 	return retval;
 }
 
-//The open function.
-static ssize_t induino_open(struct inode* inode, struct file* file){
-	printk("Opening the device file\n");
+/*The open function.*/
+static int induino_open(struct inode *inode, struct file *file)
+{
+	pr_debug("Opening the device file\n");
 	return 0;
 }
 
-//The close function.
-static ssize_t induino_release(struct inode* inode, struct file* file){
-	printk("Closing the device file\n");
+/*The close function.*/
+static int induino_release(struct inode *inode, struct file *file)
+{
+	pr_debug("Closing the device file\n");
 	return 0;
 }
 
-//The file operations structure
-static struct file_operations induino_fops={
+/*The file operations structure*/
+static const struct file_operations induino_fops = {
 	.owner = THIS_MODULE,
 	.write = induino_write,
 	.open  = induino_open,
 	.read  = induino_read,
 	.release = induino_release,
 };
-
 
 static struct usb_class_driver induino_class = {
 	.name = "usbinduino%d",
@@ -186,135 +195,139 @@ static struct usb_class_driver induino_class = {
 
 };
 
-//Called when device is inserted.
-static int induino_probe(struct usb_interface *interface, const struct usb_device_id* id){
+/*Called when device is inserted.*/
+static int induino_probe(struct usb_interface *interface,
+			const struct usb_device_id *id)
+{
 	struct usb_device *udev = interface_to_usbdev(interface);
-	struct induino_usb * dev = NULL;
-	int retval =-ENOMEM;
+	struct induino_usb *dev = NULL;
+	int retval = -ENOMEM;
 	struct usb_host_interface *iface_desc;
 	struct usb_endpoint_descriptor *endpoint;
-	int i=0;
-	int in_status=0;
-	int out_status=0;	
+	int i = 0;
+	int in_status = 0;
+	int out_status = 0;
 
-	printk("Induino board inserted\n");
+	pr_debug("Induino board inserted\n");
 
-	dev = kmalloc(sizeof(struct induino_usb),GFP_KERNEL);
+	dev = kmalloc(sizeof(struct induino_usb), GFP_KERNEL);
 
-	if(dev == NULL){
-		printk("Out of memory\n");
+	if (dev == NULL) {
+		pr_debug("Out of memory\n");
 		goto error;
 
-	}	
-		
-	memset(dev,0x00,sizeof(*dev));
+	}
+
+	memset(dev, 0x00, sizeof(*dev));
 	dev->udev = usb_get_dev(udev);
-	
 
-	//find out the bulk endpoint and store it in our local struct
-	if(interface && interface->cur_altsetting)
+	if (interface && interface->cur_altsetting) {
 		iface_desc = interface->cur_altsetting;
-	else{
-		printk("ERROR: No interface set as current interface.\n");
+	} else {
+		pr_debug("ERROR: No interface set as current interface.\n");
 		goto error;
 	}
 
-	printk("Number of Control endpoints found is %d\n",iface_desc->desc.bNumEndpoints);
+	pr_debug("Number of Control endpoints found is %d\n",
+		iface_desc->desc.bNumEndpoints);
 
-	for(i=0; i< iface_desc->desc.bNumEndpoints; i++){
+	for (i = 0; i < iface_desc->desc.bNumEndpoints; i++) {
 		endpoint = &iface_desc->endpoint[i].desc;
-				
-	
-		if(((endpoint->bEndpointAddress & USB_ENDPOINT_DIR_MASK) == USB_DIR_IN ) && ((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK) == USB_ENDPOINT_XFER_BULK)){
+
+		if (((endpoint->bEndpointAddress & USB_ENDPOINT_DIR_MASK)
+			== USB_DIR_IN) &&
+			((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK)
+			== USB_ENDPOINT_XFER_BULK)) {
 			dev->bulk_in_endpoint = endpoint;
-			in_status=1;
+			in_status = 1;
 		}
-		if(((endpoint->bEndpointAddress & USB_ENDPOINT_DIR_MASK) == USB_DIR_OUT ) && ((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK) == USB_ENDPOINT_XFER_BULK)){
+		if (((endpoint->bEndpointAddress & USB_ENDPOINT_DIR_MASK)
+			== USB_DIR_OUT) &&
+			((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK)
+			== USB_ENDPOINT_XFER_BULK)) {
 			dev->bulk_out_endpoint = endpoint;
-			out_status=1;
+			out_status = 1;
 		}
 	}
-	if(!in_status){
-		printk("Could not find any bulk-in endpoint\n");
-		goto error;
-	}
-	if(!out_status){
-		printk("Could not find the bulk-out endpoint\n");
+
+	if (!in_status) {
+		pr_debug("Could not find any bulk-in endpoint\n");
 		goto error;
 	}
 
-	
+	if (!out_status) {
+		pr_debug("Could not find the bulk-out endpoint\n");
+		goto error;
+	}
+
+
 
 	dev->bulk_in_size = dev->bulk_in_endpoint->wMaxPacketSize;
 	dev->bulk_out_size = dev->bulk_out_endpoint->wMaxPacketSize;
-	if(dev->bulk_in_size == 0 || dev->bulk_out_size == 0 ){
-		printk("Invalid max packet size for the endpoints\n");
+	if (dev->bulk_in_size == 0 || dev->bulk_out_size == 0) {
+		pr_debug("Invalid max packet size for the endpoints\n");
 		goto error;
-	}	
-	
-	printk("The max packet size for input endpoint is: %d\n",dev->bulk_in_size);
-	printk("The max packet size for output endpoint is: %d\n",dev->bulk_out_size);
-	//register the driver
-	retval = usb_register_dev(interface,&induino_class);
-	
-	if(retval){
-		printk("Could not register the induino class\n");	
-		goto error;	
 	}
-	//save the minor number 	
+
+	pr_debug("The max packet size for input endpoint is: %d\n",
+		dev->bulk_in_size);
+	pr_debug("The max packet size for output endpoint is: %d\n",
+		dev->bulk_out_size);
+
+	retval = usb_register_dev(interface, &induino_class);
+
+	if (retval) {
+		pr_debug("Could not register the induino class\n");
+		goto error;
+	}
+
 	dev->minor = interface->minor;
 
-	printk("Minor number is %d\n", dev->minor);
-	//save the local struct
-	usb_set_intfdata( interface, dev );
-	
-	return 0;
-		
+	pr_debug("Minor number is %d\n", dev->minor);
 
+	usb_set_intfdata(interface, dev);
+
+	return 0;
 error:
 	kfree(dev);
 	return retval;
-
 }
 
-
-//Called when device is removed
-static void induino_disconnect(struct usb_interface * interface){
-	
+/*Called when device is removed*/
+static void induino_disconnect(struct usb_interface *interface)
+{
 	struct induino_usb *dev;
 	dev = usb_get_intfdata(interface);
-	usb_set_intfdata(interface,NULL);
+	usb_set_intfdata(interface, NULL);
 
-	printk("Induino board removed\n");
+	pr_debug("Induino board removed\n");
 	usb_put_dev(dev->udev);
 
-	usb_deregister_dev(interface,&induino_class);
+	usb_deregister_dev(interface, &induino_class);
 
 	kfree(dev);
-
-
 }
 
-
-static struct usb_driver induino_driver={
+static struct usb_driver induino_driver = {
 	.name = "usbinduino",
 	.probe = induino_probe,
 	.disconnect = induino_disconnect,
 	.id_table = id_table,
 };
 
-static int __init usb_induino_init(void){
-	
+static int __init usb_induino_init(void)
+{
 	int retval = 0;
 	retval = usb_register(&induino_driver);
-	if(retval)
-		printk("usb_register failed. Error number %d",retval);
-	printk("Initialized the induino module\n");
+	if (retval)
+		pr_debug("usb_register failed. Error number %d", retval);
+	pr_debug("Initialized the induino module\n");
 	return retval;
 }
 
-static void __exit usb_induino_exit(void){
-	printk("Induino module exited.");
+static void __exit usb_induino_exit(void)
+{
+	pr_debug("Induino module exited.");
 	usb_deregister(&induino_driver);
 }
 
--- induino_write.c
+++ induino_write.c
@@ -6,40 +6,34 @@
 
 const char* dev="/dev/usbinduino0";
 
-void send_cmd(int fd,char* cmd, int size){
+void send_cmd(int fd,char* cmd, int size)
+{
 	int retval;
-	
 	retval = write(fd,cmd,size);
-
-	if(retval < 0)
+	if (retval < 0)
 		perror("Could not write");
-
-
 }
 
-void usage(){
-
+void usage()
+{
 	printf("induino_write data\n");
 }
 
-int main(int argc, char* argv[]){
-
+int main(int argc, char* argv[])
+{
 	int fd;
-	
-	if(argc<2){
+	if (argc < 2) {
 		usage();
 		exit(1);
 	}
 
 	
-	fd = open(dev,O_RDWR);
-	if(fd == -1){
+	fd = open(dev, O_RDWR);
+	if (fd == -1) {
 		perror("open");
 		exit(1);
 	}
 
-	send_cmd(fd,argv[1],strlen(argv[1])+1);
-	printf("size of argv 1: %d",strlen(argv[1]));
-
+	send_cmd(fd, argv[1], strlen(argv[1]) + 1);
+	printf("size of argv 1: %zu", strlen(argv[1]));
 }
-
